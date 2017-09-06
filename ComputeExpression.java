f938f252ea5ae91414c7df5d2aab6b3566ed0b85
rescuetm <31689307+rescuetm@users.noreply.github.com>
2017-09-06 02:50:41 -0700

    1

---
+++ ComputeExpression.java
@@ -0,0 +1,8 @@
+//Listing 1.3
+public class ComputeExpression //naming the class
+{ //start of the class block
+	public static void main(String[]args) //main method
+	{
+		System.out.println((10.5+2*3)/(45-3.5));
+	}
+}
